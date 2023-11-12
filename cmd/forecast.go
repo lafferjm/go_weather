5f2d1d39fc8d5067ab4e27029757c986f9fb5913
Justin Lafferty <justin.lafferty@kw.com>
2023-11-12 09:24:39 -0500

    bubble errors from get forecast

--- cmd/forecast.go
+++ cmd/forecast.go
@@ -32,20 +32,20 @@ var forecastCmd = &cobra.Command{
 	Use:   "forecast",
 	Short: "Checks the forecast for [city]",
 	Run: func(cmd *cobra.Command, args []string) {
-		forecast, err := weather.SearchLocation(City)
+		cities, err := weather.SearchLocation(City)
 		if err != nil {
 			fmt.Printf("Error looking up forecast for: %s\n", City)
 			os.Exit(1)
 		}
 
-		if len(forecast.Results) == 0 {
+		if len(cities.Results) == 0 {
 			fmt.Println("Location not found")
 			os.Exit(1)
 		}
 
 		choiceIndex := 0
-		if len(forecast.Results) > 1 {
-			choices := getChoices(forecast.Results)
+		if len(cities.Results) > 1 {
+			choices := getChoices(cities.Results)
 			prompt := &survey.Select{
 				Message: "Please choose a location:",
 				Options: choices,
@@ -53,6 +53,12 @@ var forecastCmd = &cobra.Command{
 			survey.AskOne(prompt, &choiceIndex)
 		}
 
-		weather.GetForecast(forecast.Results[choiceIndex])
+		forecast, err := weather.GetForecast(cities.Results[choiceIndex])
+		if err != nil {
+			fmt.Printf("Error looking up forecast for :%s\n", City)
+			os.Exit(1)
+		}
+
+		fmt.Println(forecast)
 	},
 }
